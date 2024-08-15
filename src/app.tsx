037cbf57bb327d41608d0f51c5f7585e3c370078
Hong Minhee <hong@minhee.org>
2024-08-15 19:07:17 +0900

    Setup process

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 import { federation } from "@fedify/fedify/x/hono";
 import { getLogger } from "@logtape/logtape";
 import { Hono } from "hono";
+import db from "./db.ts";
 import fedi from "./federation.ts";
+import type { User } from "./schema.ts";
 import { Layout, SetupForm } from "./views.tsx";
 
 const logger = getLogger("microblog");
@@ -11,12 +13,30 @@ app.use(federation(fedi, () => undefined));
 
 app.get("/", (c) => c.text("Hello, Fedify!"));
 
-app.get("/setup", (c) =>
-  c.html(
+app.get("/setup", (c) => {
+  // Check if the user already exists
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  return c.html(
     <Layout>
       <SetupForm />
     </Layout>,
-  ),
-);
+  );
+});
+
+app.post("/setup", async (c) => {
+  // Check if the user already exists
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  const form = await c.req.formData();
+  const username = form.get("username");
+  if (typeof username !== "string" || !username.match(/^[a-z0-9_-]{1,50}$/)) {
+    return c.redirect("/setup");
+  }
+  db.prepare("INSERT INTO users (username) VALUES (?)").run(username);
+  return c.redirect("/");
+});
 
 export default app;
