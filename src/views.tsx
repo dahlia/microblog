b7ad67c2b402727d4a3f6cde3b55074152e91b86
Hong Minhee <hong@minhee.org>
2024-08-15 14:03:41 +0900

    Layout

---
+++ src/views.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "hono/jsx";
+
+export const Layout: FC = (props) => (
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="color-scheme" content="light dark" />
+      <title>Microblog</title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+      />
+    </head>
+    <body>
+      <main class="container">{props.children}</main>
+    </body>
+  </html>
+);
