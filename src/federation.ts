3ac70696dc99e4c420e1141039668445f894e792
Hong Minhee <hong@minhee.org>
2024-08-23 02:28:45 +0900

    Listen to Create(Note)

--- src/federation.ts
+++ src/federation.ts
@@ -1,5 +1,6 @@
 import {
   Accept,
+  Create,
   Endpoints,
   Follow,
   Note,
@@ -198,6 +199,21 @@ federation
       )
       `,
     ).run(followingId, parsed.handle);
+  })
+  .on(Create, async (ctx, create) => {
+    const object = await create.getObject();
+    if (!(object instanceof Note)) return;
+    const actor = create.actorId;
+    if (actor == null) return;
+    const author = await object.getAttribution();
+    if (!isActor(author) || author.id?.href !== actor.href) return;
+    const actorId = (await persistActor(author))?.id;
+    if (actorId == null) return;
+    if (object.id == null) return;
+    const content = object.content?.toString();
+    db.prepare(
+      "INSERT INTO posts (uri, actor_id, content, url) VALUES (?, ?, ?, ?)",
+    ).run(object.id.href, actorId, content, object.url?.href);
   });
 
 federation
